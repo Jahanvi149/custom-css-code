0c03f1b2f5ac235b9487e1c44b525584e4ef45ba
jahanvi149 <memdavadijahanvi@gmail.com>
2024-08-13 21:35:28 +0530

    change

--- js/custom.js
+++ js/custom.js
@@ -3,23 +3,6 @@
 var $ = jQuery;
 
 
-
-// (function ($){
-//     'use strict';
-//     jQuery(document).ready(function() {
-//         jQuery("header .hamburger-menu").click(function(){
-//             jQuery("header .menu-container").slideToggle(),
-//             jQuery("header .hamburger-menu").toggleClass("mobile-menu-active")
-//         }),
-//         jQuery("header .hamburger-menu").on("click", function() {
-//             jQuery(this).hasClass("active") ? jQuery(this).removeClass("active") : jQuery(this).addClass("active")
-//         })
-
-    
-//     });
-// })(jQuery);
-
-
 (function ($){
     'use strict';
     jQuery(document).ready(function() {
@@ -35,15 +18,3 @@ var $ = jQuery;
     });
 })(jQuery);
 
-
-
-// (function ($){
-// $(document).ready(function() {
-  
-//   $(".hamburger-menu").click(function() {
-//     $("body").toggleClass("active");
-//   })
-  
-  
-// });
-// })(jQuery);
